a05599c97cf73b8dd41a7f84ca81ac5c67e1e18d
suhaibsuhai <suhaibhamza300@gmail.com>
2024-11-30 06:56:34 +0000

    changed port

--- frontend/src/services/item.service.js
+++ frontend/src/services/item.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://sprinboot-backend.azurewebsites.net/items';
+const apiUrl = 'http://localhost:8082/items';
 
 const getItems = () => {
   return axios.get(apiUrl);
